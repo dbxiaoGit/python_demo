b46776b2bc04e93e7cb1d3dc8a759cd95ea886c7
豆比 <136187300@qq.com>
2017-12-12 14:45:38 +0800

    1
    1

---
+++ test_log.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S',
+                filename='myapp.log',
+                filemode='w')
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+
+logging.debug('This is debug message')
+logging.info('This is info message')
+logging.warning('This is warning message')
