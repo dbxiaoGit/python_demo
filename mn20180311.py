4cd32f8f2c4c08fc91b639f32dbedd425cff266c
豆比 <136187300@qq.com>
2018-03-11 18:59:14 +0800

    Update mn20180311.py

--- mn20180311.py
+++ mn20180311.py
@@ -1,22 +1,18 @@
 from com.android.monkeyrunner import MonkeyDevice,MonkeyRunner
+import traceback
 
 def auto_touch():
 	try:
 		driver = MonkeyRunner.waitForConnection(30,'0123456789ABCDEF')
-
 		print type(driver)
 		w = driver.getProperty('display.width')
 		h = driver.getProperty('display.height')
 		print int(w)
-
 		cg_x = int(w)*1600/1920
 		cg_y = int(h)*960/1080
-
 		print 'cg(%d,%d)'%(cg_x,cg_y)
 		count = 1
-
 		driver.startActivity(component = "com.tencent.tmgp.sgame/com.tencent.tmgp.sgame.SGameActivity")
-
 		while True :
 				print 'touch count:%d ,touch postion x:%d ,y:%d'%(count,cg_x,cg_y)
 				driver.wake()
