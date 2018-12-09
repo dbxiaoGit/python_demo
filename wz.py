29ad1de988300a0ae6eaee10533d908b589dddff
豆比 <136187300@qq.com>
2018-12-09 20:56:07 +0800

    Update wz.py

--- wz.py
+++ wz.py
@@ -1,40 +1,37 @@
 from com.android.monkeyrunner import MonkeyDevice,MonkeyRunner
+import traceback
 
-driver = MonkeyRunner.waitForConnection(30,'0123456789ABCDEF')
-print type(driver)
-w = driver.getProperty('display.width')
-h = driver.getProperty('display.height')
-print int(w)
-
-cg_x = int(w)*1450/1920
-cg_y = int(h)*920/1080
-
-pm_x = int(w)/2
-pm_y = int(h)*1020/1080
-
-zl_x = int(w)*1450/1920
-zl_y = int(h)*990/1080
-
-print 'cg(%d,%d),pm(%d,%d),zl(%d,%d)'%(cg_x,cg_y,pm_x,pm_y,zl_x,zl_y)
-
-
-driver.startActivity(component = "com.tencent.tmgp.sgame/com.tencent.tmgp.sgame.SGameActivity")
-#MonkeyRunner.sleep(5)
-while True :
-	driver.wake()
-	print 'step1:press chuangguan'
-	driver.touch(cg_x,cg_y,MonkeyDevice.DOWN_AND_UP)
-	MonkeyRunner.sleep(5)
-	
-	print 'step2:sleep 100s'
-	MonkeyRunner.sleep(100)
-	
-	print 'step3:press screen'
-	driver.touch(pm_x,pm_y,MonkeyDevice.DOWN_AND_UP)
-	MonkeyRunner.sleep(1)
-	driver.touch(pm_x,pm_y,MonkeyDevice.DOWN_AND_UP)
-	MonkeyRunner.sleep(5)
-	
-	print 'step4:press play again'
-	driver.touch(zl_x,zl_y,MonkeyDevice.DOWN_AND_UP)
-	MonkeyRunner.sleep(5)
+class AutoPlayer:
+	def __init__(self):
+		self.count = 1
+		self.driver = MonkeyRunner.waitForConnection("MKJNW17C19000245",30)
+		print type(self.driver)
+		self.screen_width = self.driver.getProperty('display.width')
+		self.screen_height = self.driver.getProperty('display.height')
+		print int(self.screen_width )
+		print int(self.screen_height)
+		self.point_x = int(self.screen_width)*1600/1920
+		self.point_y = int(self.screen_height)*960/1080
+		print 'cg(%d,%d)'%(self.point_x,self.point_y)
+		self.driver.startActivity(component = "com.tencent.tmgp.sgame/com.tencent.tmgp.sgame.SGameActivity")
+	def auto_play(self):
+		while True :
+			try:
+				#self.driver = MonkeyRunner.waitForConnection()
+				print 'touch count:%d ,touch postion x:%d ,y:%d'%(self.count,self.point_x,self.point_y)
+				self.driver.touch(self.point_x,self.point_y,MonkeyDevice.DOWN_AND_UP)
+				MonkeyRunner.sleep(1)
+				self.count = self.count+1
+			except BaseException,e:
+				print traceback.print_exc()
+if __name__ == '__main__':
+	auto_player_1 = ''
+	while True:
+		try:
+			auto_player_1 = AutoPlayer()
+			auto_player_1.auto_play()
+		except BaseException,e:
+			print traceback.print_exc()
+		finally:
+			auto_player_1 = AutoPlayer()
+			auto_player_1.auto_play()
